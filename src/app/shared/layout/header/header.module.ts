c241456f63499695aec56508caae87d411be38f3
Nurdaulet <nurik93sat@gmail.com>
2022-04-13 00:05:40 +0600

    App header

--- src/app/shared/layout/header/header.module.ts
+++ src/app/shared/layout/header/header.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from "@shared/shared.module";
 import { RouterModule } from "@angular/router";
 
 import { HeaderComponent } from './header.component';
@@ -10,6 +11,7 @@ import { HeaderComponent } from './header.component';
   ],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule
   ],
   exports: [
